cae411f24ce87a49a64a8b257d2031ccfd5a3d8d
nickhir <hirschmueller.nick@gmail.com>
2024-03-27 20:36:24 +0000

    init commit

---
+++ helper_functions.R
@@ -0,0 +1,239 @@
+get_assoc_pval_categorical <- function(x, y) {
+    if (n_distinct(x) == 2) {
+        p_val <- broom::tidy(wilcox.test(y ~ x))[, "p.value", drop=TRUE]
+    } else {
+        p_val <- broom::tidy(kruskal.test(y ~ x))[, "p.value", drop=TRUE]
+    }
+    return(p_val)
+}
+
+get_assoc_pval_continous <- function(x, y) {
+    p_val <- broom::tidy(lm(y ~ x))[2, "p.value", drop=TRUE] # pick second row, the first will be the intercept.
+    return(p_val)
+}
+
+#' Calculate covariate correlations with components
+#'
+#' This function calculates the association between specified covariates and components in the input dataframe.
+#'
+#' @param df Input dataframe containing covariates and components
+#' @param covars Character vector specifying the covariates to analyze
+#' @param components Character vector specifying the components to analyze (default: c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6"))
+#' @return A list containing:
+#' \item{adjusted}{Matrix of adjusted p-values for the associations between covariates and components}
+#' \item{raw}{Data frame of raw p-values for each covariate-component pair}
+#' \item{heatmap}{Heatmap visualizing the adjusted p-values}
+#' @examples
+#' df <- data.frame(covar1 = rnorm(100), covar2 = rnorm(100), PC1 = rnorm(100), PC2 = rnorm(100))
+#' covar_correlation(df, c("covar1", "covar2"), c("PC1", "PC2"))
+covar_correlation <- function(df, covars, components = c("PC1", "PC2", "PC3", "PC4", "PC5", "PC6")) {
+    require(ComplexHeatmap)
+    require(circlize)
+    # check if specified covars exist in input dataframe
+    if (!all(covars %in% colnames(df))) {
+        missing_covars <- covars[!covars %in% colnames(df)]
+        stop(str_interp("The indicated covariates do not occur in the dataframe: ${missing_covars}"))
+    }
+
+    # check if all components are in input dataframe
+    if (!all(components %in% colnames(df))) {
+        missing_components <- components[!components %in% colnames(df)]
+        stop(str_interp("The indicated components do not occur in the dataframe: ${missing_components}"))
+    }
+
+    # go through covars and calculate association with all specified components
+    covar_p_vals <- lapply(setNames(covars, covars), function(covar) {
+        print(str_interp("Processing covar ${covar}"))
+        tmp_cov <- df[, covar]
+        if (n_distinct(tmp_cov) == 1) {
+            stop(str_interp("${covar} only has a single level."))
+        }
+
+        # go through the different components
+        component_p_vals <- lapply(setNames(components, components), function(component) {
+            tmp_component <- df[, component]
+            # if variable is continous, use correlation as an estimate of association.
+            # if variable is categorical (as indicated by factor), use either a wilcoxon test (if 2 categories) or a kruskal.test
+            if (is.factor(tmp_cov)) {
+                p <- get_assoc_pval_categorical(x = tmp_cov, y = tmp_component)
+            } else {
+                p <- get_assoc_pval_continous(x = tmp_cov, y = tmp_component)
+            }
+            return(p)
+        })
+    })
+    # do global p_value adjustment
+    adjusted_pvals <- p.adjust(unlist(covar_p_vals), method = "fdr") # Adjust using FDR method
+    adjusted_matrix <- matrix(adjusted_pvals,
+        byrow = T,
+        nrow = length(covars), ncol = length(components)
+    ) %>%
+        data.frame()
+    rownames(adjusted_matrix) <- covars
+    colnames(adjusted_matrix) <- components
+
+    heatmap <- Heatmap(adjusted_matrix %>% as.matrix(),
+        cluster_rows = FALSE,
+        cluster_columns = FALSE,
+        row_names_side = "left",
+        column_names_gp = grid::gpar(fontsize = 12, fontface = "bold"),
+        row_names_gp = grid::gpar(fontsize = 12, fontface = "bold"),
+        border = "black",
+        layer_fun = function(j, i, x, y, width, height, fill) {
+            # since grid.text can also be vectorized
+            grid.text(round(pindex(adjusted_matrix %>% as.matrix(), i, j), 2), x, y,
+                gp = gpar(fontsize = 11, col = "black")
+            )
+        },
+        col = colorRamp2(
+            c(0, 0.05, 1),
+            c("red", "grey", "blue")
+        ),
+        heatmap_legend_param = list(
+            title = "adj. p",
+            at = c(0, 0.05, 1)
+        )
+    )
+
+    return(list(
+        adjusted = adjusted_matrix,
+        raw = bind_rows(covar_p_vals) %>% data.frame() %>% magrittr::set_rownames(covars),
+        heatmap=heatmap
+    ))
+}
+
+
+
+plot_continuous_covar <- function(df, covar, PC, x_pos = "left",
+                                  y_pos = "bottom") {
+    p <- ggscatter(df, x = PC, y = covar, add = "reg.line") +
+        stat_cor(label.x.npc = x_pos, label.y.npc = y_pos) +
+        xlab(paste(PC, ":", round(pca_data$variance[PC], 2), "% variance")) +
+        theme_Publication()
+    return(p)
+}
+
+#' Plot categorical covariate with principal components
+#'
+#' This function generates boxplots to visualize the relationship between a categorical covariate and specified principal components in the input dataframe.
+#'
+#' @param df Input dataframe containing the covariate and principal components
+#' @param covar Character string specifying the categorical covariate to plot
+#' @param PC Character vector specifying the principal components to analyze
+#' @return A list of ggplot2 objects representing boxplots for each principal component with the categorical covariate
+#' @examples
+#' df <- data.frame(covar = factor(rep(c("A", "B"), 50)), PC1_explained_var = runif(100), PC2_explained_var = runif(100))
+#' plot_categorical_covar(df, "covar", c("PC1", "PC2"))
+plot_categorical_covar <- function(df, covar, PC, xlab = waiver(), ylab = waiver()) {
+    plots <- lapply(PC, function(tmp_pcs) {
+        # get the explained variation of the PC. for that we have to get the correct column
+        if (n_distinct(df[, covar]) == 2) {
+            p <- ggplot(df, aes_string(x = covar, y = tmp_pcs, fill = covar)) +
+                geom_boxplot(width = 0.6) +
+                geom_signif(comparisons = list(levels(df[, covar]))) +
+                theme_Publication() %+%
+                theme(legend.position = "none") +
+                ylab(ylab) +
+                xlab(xlab) +
+                ylim(min(df[, tmp_pcs]) - 3, max(df[, tmp_pcs]) + 50)
+        } else {
+            # calc p value with the kruskal test
+            p.val <- round(broom::tidy(kruskal.test(get(tmp_pcs) ~ get(covar), data = df))$p.value, 5)
+            p <- ggplot() +
+                geom_boxplot(data = df, aes_string(x = covar, y = tmp_pcs, fill = covar), width = 0.6) +
+                geom_label(aes(
+                    x = median(1:n_distinct(df[, covar])),
+                    y = max(df[, tmp_pcs]) + 1,
+                    label = paste0("p value = ", p.val)
+                )) +
+                theme_Publication() %+%
+                theme(legend.position = "none") +
+                ylab(ylab) +
+                xlab(xlab) 
+        }
+        return(p)
+    })
+    return(plots)
+}
+
+
+
+
+#' Density PCA Plot Function
+#'
+#' This function creates a PCA plot with density plots on the top and right side.
+#'
+#' @param df A data frame containing the data to be plotted.
+#' @param x_variable The name of the variable to be plotted on the x-axis.
+#' @param y_variable The name of the variable to be plotted on the y-axis.
+#' @param color_by The name of the variable by which the points should be colored.
+#' @param xlab The label for the x-axis. Defaults to a waiver.
+#' @param ylab The label for the y-axis. Defaults to a waiver.
+#' @param shape_by The name of the variable by which the points should be shaped. Defaults to NA.
+#' @param colors A vector of colors to be used in the plot. If NA, distinct colors will be generated. Defaults to NA.
+#' @param linewidth The width of the lines in the density plots. Defaults to 0.8.
+#' @param pt_size The size of the points in the PCA plot. Defaults to 3.
+#' @param show_density Logical indicating whether or not to show the density plots. Defaults to TRUE.
+#' @param density_plot_ratio The ratio of the size of the density plots to the size of the PCA plot. Defaults to 0.3.
+#' @param alpha The transparency of the density plots. Defaults to 0.3.
+#'
+#' @return A ggplot object containing the PCA plot and density plots.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data <- data.frame(x = rnorm(100), y = rnorm(100), condition = sample(c("A","B"), 100, replace=T))
+#' density_pca(data, "x", "y", "condition")
+#' }
+density_pca <- function(
+    df, x_variable, y_variable, color_by, xlab = waiver(), ylab = waiver(), shape_by = NA, colors = NA,
+    linewidth = 0.8, pt_size = 3, show_density = TRUE, density_plot_ratio = 0.3, alpha = 0.3) {
+    if (any(is.na(colors))) {
+        colors = chameleon::distinct_colors(n_distinct(df[, color_by]))$name
+    }
+
+    # determine the colors
+    pmain <- ggplot(df, aes(!!sym(x_variable), !!sym(y_variable),
+        color = !!sym(color_by),
+    )) +
+        geom_point(size = pt_size, alpha=0.9) +
+        xlab(xlab) +
+        ylab(ylab) +
+        theme_Publication() +
+        scale_color_manual(values = colors)
+    
+    if (!is.na(shape_by)) {
+        pmain <- pmain + aes(shape = !!sym(shape_by))
+    }
+    
+    if (!show_density) {
+        return(pmain)
+    }
+
+    xlim.update <- layer_scales(pmain)$x$get_limits()
+    ylim.update <- layer_scales(pmain)$y$get_limits()
+
+    ptop <- ggplot(data = df, aes(x = !!sym(x_variable), fill = !!sym(color_by), y=..scaled..)) +
+        geom_density(linewidth = linewidth, alpha = alpha) +
+        theme_void() %+%
+        theme(legend.position = "none") +
+        scale_x_continuous(limits = xlim.update) +
+        scale_fill_manual(values = colors)
+
+    pright <- ggplot(data = df, aes(x = !!sym(y_variable), fill = !!sym(color_by), y=..scaled..)) +
+        geom_density(linewidth = linewidth, alpha = alpha) +
+        coord_flip() +
+        theme_void() %+%
+        theme(legend.position = "none") +
+        scale_fill_manual(values = colors)
+
+
+
+    layout <- "
+    A#
+    BC
+    "
+    final_plot <- wrap_plots(ptop, pmain, pright) +
+        plot_layout(design = layout, widths = c(1, density_plot_ratio), heights = c(density_plot_ratio, 1))
+    return(final_plot)
+}
---
+++ plot_theme.R
@@ -0,0 +1,64 @@
+theme_Publication <- function(base_size=14) {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.2), hjust = 0.5),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.2),
+            axis.text = element_text(), 
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.key.size= unit(0.3, "cm"),
+            legend.spacing = unit(0, "cm"),
+            legend.title = element_text(face="bold",size=13),
+            legend.text = element_text(size=12),
+            plot.margin=unit(c(3,3,3,3),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+    ))
+  
+}
+
+small_axis <- function(label = NULL, fontsize = 7, arrow_length = 25, fix_coord = FALSE, arrow_offset=6,
+                       arrow_spec = grid::arrow(ends = "both", type = "closed", angle = 20, length = unit(arrow_length / 7, units)),
+                       units = "mm", ...){
+  coord <- if(fix_coord){
+    coord_fixed(clip = "off", ...)
+  }else{
+    NULL
+  }
+  if (is.null(label)){
+    label <- "UMAP"
+  }
+  lines <- annotation_custom(grid::polylineGrob(x = unit(c(arrow_offset, arrow_offset, arrow_length), units), 
+                                                y = unit(c(arrow_length, arrow_offset, arrow_offset), units), 
+                                                gp = grid::gpar(fill = "black"),
+                                                arrow = arrow_spec))
+  text <- if(! is.null(label)){
+    list(annotation_custom(grid::textGrob(label = paste0(label,"1"), gp = grid::gpar(fontsize = fontsize, fontface="bold"),
+                                          x = unit(arrow_offset, units), y = unit(arrow_offset, units), 
+                                          hjust = 0, vjust = 1.4)),
+         annotation_custom(grid::textGrob(label = paste0(label,"2"), rot=90, gp = grid::gpar(fontsize = fontsize, fontface="bold"),
+                                          x = unit(arrow_offset, units), y = unit(arrow_offset, units), 
+                                          hjust = 0, vjust = -0.45)))
+    
+  }else{
+    NULL
+  }
+  
+  theme_plot <- theme_void() %+%
+    theme(plot.title = element_text(hjust=0.5, face="bold",size=12))
+  
+  list(coord, lines, text, theme_plot)
+}
