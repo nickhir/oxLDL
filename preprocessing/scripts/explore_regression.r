d5e6e96d3ddce14f811b5a87b5a3a7e86c8c8cd8
nickhir <hirschmueller.nick@gmail.com>
2024-04-10 12:02:19 +0100

    update2

--- preprocessing/scripts/explore_regression.r
+++ preprocessing/scripts/explore_regression.r
@@ -1,21 +1,39 @@
+library(DESeq2)
+library(PCAtools)
+library(httpgd)
+library(tidyverse)
+library(here)
+source(here("plot_theme.R"))
+source(here("helper_functions.R"))
+
 # this function regresses SVs out of genomic data
 cleaningP = function(y, mod, svaobj, P = ncol(mod)) {
-        X = cbind(mod, svaobj$sv)
-        Hat = solve(t(X) %*% X) %*% t(X)
-        beta = (Hat %*% t(y))
-        cleany = y - t(as.matrix(X[, -c(1:P)]) %*% beta[-c(1:P), ])
-        return(cleany)
+    X = cbind(mod, svaobj$sv)
+    Hat = solve(t(X) %*% X) %*% t(X)
+    beta = (Hat %*% t(y))
+    cleany = y - t(as.matrix(X[, -c(1:P)]) %*% beta[-c(1:P), ])
+    return(cleany)
 }
 
 
-mod <- model.matrix(~sampletype, colData(dds))
 
-# sva with default # of SVs
+
+
+ solve(t(X) %*% X) %>% dim()
+test
+
+
+
+
+
+dds <- readRDS(here("QC", "results", "dds_libsize_outliers_removed.rds"))
+mdata <- colData(dds) %>% data.frame()
+mod <- model.matrix(~sampletype, colData(dds))
 svaobj = readRDS(here("QC", "results", str_interp("sva_12SVs.rds")))
-p <- assay(vsd_data)
+norm_data <- assay(vst(dds, blind=TRUE))
 
 # regress out SVs
-cleanp = cleaningP(p, mod, svaobj)
+cleanp = cleaningP(norm_data, mod, svaobj)
 
 # only keep top 40%
 # Calculate standard deviation for each feature
@@ -30,12 +48,12 @@ selected_indices <- which(std_dev >= threshold)
 # Subset the original data to keep only the selected features
 selected_data <- cleanp[selected_indices, ]
 
-pca = prcomp(t(selected_data))
+pca = prcomp(t(cleanp))
 pcaVars=signif(((pca$sdev)^2)/(sum((pca$sdev)^2)),3)*100
 signed = ifelse(max(pca$x[, 2] > 70), 1, -1) # same sign across plots
 
 plot(pca$x[, 1], pca$x[, 2],
-        pch = 19,
+        pch = 19, col = mdata$state,
         main = "Figure 1A - No SVA", cex = 1.1,
         ylim = c(-80, 100), cex.axis = 1.5, cex.lab = 1.5, 
         xlab = paste("PC1:", pcaVars[1], "% of Variance Explained"), ylab = paste("PC2:", pcaVars[2], "% of Variance Explained")
